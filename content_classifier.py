1c44080973145681da6a3dcaf5109cc6ba701ba0
Timi <tw1838@nyu.edu>
2023-12-23 15:36:19 -0800

    Modified codes to stop when converged.
    Modify the cnentent_classifer.py to stop when converged.

--- content_classifier.py
+++ content_classifier.py
@@ -1,15 +1,17 @@
 import pandas as pd
-import tensorflow as tf
+import tensorflow
 from keras.models import Sequential
 from keras.layers import Dense
 from keras.layers import LSTM
 from keras.layers import Dropout
 from keras.preprocessing import sequence
+from keras.callbacks import EarlyStopping
 from sklearn.model_selection import train_test_split
 import os
 import glob
-
 import numpy as np
+
+# Prepare the datasets
 data_set = pd.DataFrame()
 all_df = []
 folder_path = "./embeddings_2/"
@@ -26,21 +28,33 @@ X_train_3d = np.stack([x.reshape(-1, 1) for x in X_train_array])
 X_test_array = X_test.to_numpy()
 X_test_3d = np.stack([x.reshape(-1, 1) for x in X_test_array])
 
+# Set stop when converged
+early_stopping = EarlyStopping(
+    monitor='val_loss',     # The metric to monitor (e.g., validation loss)
+    min_delta=0.001,        # Minimum change to qualify as an improvement
+    patience=10,            # How many epochs to wait for improvement
+    verbose=1,              # Verbose output
+    mode='min',             # The mode ('min' for minimizing, 'max' for maximizing)
+    restore_best_weights=True  # Restore model weights from the epoch with the best value of the monitored quantity
+)
+
+# Train the model
 model = Sequential()
 model.add(LSTM(100))
 model.add(Dropout(0.2))
 model.add(Dense(1, activation='sigmoid'))
 model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
 
-history = model.fit(X_train_3d, y_train, epochs=50, batch_size=64,shuffle=True, validation_data=(X_test_3d, y_test))
-# Access the history data
-training_loss = history.history['loss']
-training_accuracy = history.history['accuracy']
-
-# If validation data was used, you can also access validation loss and accuracy
-validation_loss = history.history.get('val_loss')
-validation_accuracy = history.history.get('val_accuracy')
+history = model.fit(
+    X_train_3d, 
+    y_train, 
+    epochs=50, 
+    batch_size=64,
+    shuffle=True, 
+    validation_data=(X_test_3d, y_test),
+    callbacks=[early_stopping]
+    )
 
 history_df = pd.DataFrame(history.history)
 history_df['epoch'] = range(1, len(history_df) + 1)
-history_df.to_csv('./results/training_history_fullsets_1.csv', index=False)
+history_df.to_csv('./results/history_fullsets_1.csv', index=False)
