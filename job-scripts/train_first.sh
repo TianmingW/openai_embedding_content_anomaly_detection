c2495d5fc189996793d70e3fe8127870d0e2049f
Timi <tw1838@nyu.edu>
2023-12-26 19:37:41 -0800

    Extended time to 2 days run fourth time.

--- job-scripts/train_first.sh
+++ job-scripts/train_first.sh
@@ -2,8 +2,8 @@
 #SBATCH --mem=32G
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=8
-#SBATCH --time=24:0:0
-#SBATCH --mail-user=tianmingwang@uvic.ca
+#SBATCH --time=48:0:0
+#SBATCH --mail-user=wangmaine@gmail.com
 #SBATCH --mail-type=ALL
 
 cd $project/embedding_detection/openai_embedding_content_anomaly_detection
